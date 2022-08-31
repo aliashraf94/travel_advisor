bb806240e51737be42d3b021cb89358bd7ee8190
Ali Ashraf <adaydreamer10@gmail.com>
2022-09-01 01:42:12 +0200

    Added Map component

--- src/componenets/Map/Map.jsx
+++ src/componenets/Map/Map.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { LocationOnOutlinedIcon } from '@material-ui/icons/LocationOnOutlined';
+import { Rating } from '@material-ui/lab';
+
+import useStyles from './styles'
+
 
 const Map = () => {
+
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)')
+    const coordinates = { lat: 41.3874, lng: 2.1686 }
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer} >
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: "AIzaSyBlkGLtPF_q7VRePyx1r8kZpvCpcrQcD_4" }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
