a47efc5643304e34db3322bf5e15198fadfb614e
Ali Ashraf <adaydreamer10@gmail.com>
2022-08-31 22:02:36 +0200

    Navbar using @material-ui

--- src/componenets/Header/Header.jsx
+++ src/componenets/Header/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar,Toolbar,Typography,InputBase,Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search'
+import useStyles from './styles.js'
 
 const Header = () => {
+    const classes = useStyles()
     return (
-        <h1>Header</h1>
+        <AppBar position='static'>
+            <Toolbar className={classes.toolbar}>
+                <Typography variant='h5' className={classes.title}>
+                    Travel Advisor
+                </Typography>
+                <Typography variant='h6' className={classes.title}>
+                    Explore New Places
+                </Typography>
+                <Box display="flex">    
+                {/* <Autocomplete> */}
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon} >
+                            <SearchIcon />
+                        </div>
+                        <InputBase placeholder="Search ..." classes={{ root: classes.inputRoot, input: classes.inputInput  }} />
+                    </div>
+                {/* </Autocomplete> */}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 }
 
